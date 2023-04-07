106feabdd9d90caf4d11262b1761b30f83e4d921
ArcherGilgam3sh <83524709+ArcherGilgam3sh@users.noreply.github.com>
2023-04-07 19:00:49 +0800

    course page components

---
+++ components/PageComponents.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="page-component">
+    <table>
+      <thead>
+        <tr>
+          <th>Event ID</th>
+          <th>Course ID</th>
+          <th>Semester</th>
+          <th>Instructor</th>
+          <th>Event Name</th>
+          <th>Start Time</th>
+          <th>End Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="event in events" :key="event.eventId">
+          <td>{{ event.eventId }}</td>
+          <td>{{ event.courseId }}</td>
+          <td>{{ event.semester }}</td>
+          <td>{{ event.instructor }}</td>
+          <td>{{ event.eventName }}</td>
+          <td>{{ event.startTime }}</td>
+          <td>{{ event.endTime }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      events: [],
+    };
+  },
+  created() {
+    this.fetchEvents();
+  },
+  methods: {
+    async fetchEvents() {
+      const response = await fetch("http://10.27.119.221:8181/courseEvent/listCourseEvent/{course_id}?courseID=1");
+      const data = await response.json();
+      this.events = data;
+    },
+  },
+};
+</script>
+
+<style>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th, td {
+  border: 1px solid black;
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  background-color: #f2f2f2;
+}
+</style>
