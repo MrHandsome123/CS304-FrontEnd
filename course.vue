a728ae5487b390cb887c9d465fd24ace8bd62a8a
ArcherGilgam3sh <83524709+ArcherGilgam3sh@users.noreply.github.com>
2023-04-07 18:59:59 +0800

    Create course.vue
    Course Page

---
+++ course.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <page-component />
+  </div>
+</template>
+
+<script>
+import PageComponent from './components/PageComponent.vue';
+
+export default {
+  components: {
+    PageComponent,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+  flex: 1;
+}
+</style>
