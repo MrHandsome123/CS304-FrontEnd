401ab3d2d64399b357acb1b49dc660bc2933eb66
ArcherGilgam3sh <83524709+ArcherGilgam3sh@users.noreply.github.com>
2023-05-27 20:41:26 +0800

    Update index.js
    Update Routers

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import Main from "../components/MainPage.vue";
 import UserLogin from "../components/UserLogin.vue";
 import Course from "../components/CoursePage.vue";
+import UserRegister from "../components/UserRegister.vue";
+import EditCourse from "../components/EditCourse.vue"
 
 const routes = [
   {
@@ -19,6 +21,16 @@ const routes = [
     name: "course",
     component: Course,
   },
+  {
+    path: "/register",
+    name: "register",
+    component: UserRegister
+  },
+  {
+    path: "/editCourse",
+    name: "editCourse",
+    component: EditCourse
+  }
 ];
 
 const router = createRouter({
